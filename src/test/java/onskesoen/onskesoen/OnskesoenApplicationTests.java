6f05c941ef07927effbcdcbc32fe6928ec61d03c
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-04-08 10:34:10 +0200

    onske

---
+++ src/test/java/onskesoen/onskesoen/OnskesoenApplicationTests.java
@@ -0,0 +1,13 @@
+package onskesoen.onskesoen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnskesoenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
