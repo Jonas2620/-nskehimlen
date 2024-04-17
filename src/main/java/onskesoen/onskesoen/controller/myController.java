eb6387e8e1cf173b36d374effe8c4385e7dc414c
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-04-17 14:14:55 +0200

    Ønskeliste

--- src/main/java/onskesoen/onskesoen/controller/myController.java
+++ src/main/java/onskesoen/onskesoen/controller/myController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -27,16 +28,44 @@ public class myController {
     @GetMapping("/onskeliste")
     public String ønskeliste(Model model){
         List<Ønskeliste> ønskeList = ønskelisteService.fetchAll();
-        model.addAttribute("ønskeliste", ønskeList);
+        System.out.println(ønskeList); // Tilføjet linje til udskrivning af ønskeliste
+        model.addAttribute("onskelister", ønskeList);
         return "onskeliste";
+
     }
     @GetMapping("/nyØnskesø")
     public String create(){
         return "nyØnskesø";
     }
     @PostMapping("/tilføj")
-    public String createNew(@ModelAttribute Ønskeliste ønskeliste){
+    public String createNew(@ModelAttribute("ønskeliste") Ønskeliste ønskeliste){
         ønskelisteService.addØnskelist(ønskeliste);
         return "redirect:/onskeliste";
     }
+
+
+@GetMapping("/viewOne/{id}")
+public String viewOne(@PathVariable("id") int id, Model model){
+    model.addAttribute("ønskeliste", ønskelisteService.findØnskelisteId(id));
+    return "viewone";
+}
+@GetMapping("/deleteOne/{id}")
+public String deleteOne(@PathVariable("id") int id){
+    boolean deleted = ønskelisteService.deleteØnskeliste(id);
+    if (deleted){
+        return "redirect:/onskeliste";
+    }else {
+        return "redirect:/onskeliste";
+    }
+}
+@GetMapping("/updateOne/{id}")
+public String updateOne(@PathVariable("id") int id, Model model){
+    model.addAttribute("ønskeliste", ønskelisteService.deleteØnskeliste(id));
+    return "updateOne";
+}
+@PostMapping("/updateØnskeliste")
+public String updatePerson(@ModelAttribute Ønskeliste ønskeliste){
+    ønskelisteService.updateØnskeliste(ønskeliste.getWishlist_id(), ønskeliste);
+    return "redirect:/onskeliste";
+}
 }
