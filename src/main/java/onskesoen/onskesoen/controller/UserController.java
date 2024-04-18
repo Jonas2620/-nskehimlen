93fc24a4a2bb1e27fa9b915f8538802080500be3
Jonas Vistrup <Jonas_vistrup@live.dk>
2024-04-18 14:22:22 +0200

    Fejlrettelser

--- src/main/java/onskesoen/onskesoen/controller/UserController.java
+++ src/main/java/onskesoen/onskesoen/controller/UserController.java
@@ -4,25 +4,31 @@ import onskesoen.onskesoen.model.User;
 import onskesoen.onskesoen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/register")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
+    @GetMapping
+    public String showRegistrationForm() {
+        return "register";
+    }
+
+    @PostMapping
     public String registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
         user.setEmail(email);
         userService.registerUser(user);
-        return "redirect:user/login";
+        return "redirect:/login";
     }
 
     @PostMapping("/login")
@@ -30,7 +36,8 @@ public class UserController {
         if (userService.loginUser(username, password)) {
             return "redirect:/forside";
         } else {
-            return "redirect:user/login?error=true";
+            return "redirect:/login?error=true";
         }
     }
 }
+
