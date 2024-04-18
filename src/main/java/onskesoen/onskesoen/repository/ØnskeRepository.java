5f4d5d3ca36dad474a2f4d9821fc9ebadeeb96dc
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-04-18 15:02:03 +0200

    Tilføj ønsker failed :_(

--- src/main/java/onskesoen/onskesoen/repository/ØnskeRepository.java
+++ src/main/java/onskesoen/onskesoen/repository/ØnskeRepository.java
@@ -16,23 +16,23 @@ public class ØnskeRepository {
     private JdbcTemplate template;
 
     public void tilføjØnske(Ønske ønske) {
-        String sql = "INSERT INTO ønske (wishlist_id, ønske_navn) VALUES (?, ?)";
+        String sql = "INSERT INTO ønskesøen.ønsker (wishlist_id, ønske_navn) VALUES (?, ?)";
         template.update(sql, ønske.getWishlistId(), ønske.getØnskeNavn());
     }
 
     public List<Ønske> findØnskerTilWishlist(int wishlistId) {
-        String sql = "SELECT * FROM ønske WHERE wishlist_id = ?";
+        String sql = "SELECT * FROM ønskesøen.ønsker WHERE wishlist_id = ?";
         RowMapper<Ønske> rowMapper = new BeanPropertyRowMapper<>(Ønske.class);
         return template.query(sql, rowMapper, wishlistId);
     }
 
     public boolean sletØnske(int ønskeId) {
-        String sql = "DELETE FROM ønske WHERE ønske_id = ?";
+        String sql = "DELETE FROM ønskesøen.ønsker WHERE ønske_id = ?";
         return template.update(sql, ønskeId) > 0;
     }
 
     public void opdaterØnske(Ønske ønske) {
-        String sql = "UPDATE ønske SET ønske_navn = ? WHERE ønske_id = ?";
+        String sql = "UPDATE ønskesøen.ønsker SET ønske_navn = ? WHERE ønske_id = ?";
         template.update(sql, ønske.getØnskeNavn(), ønske.getId());
     }
 }
